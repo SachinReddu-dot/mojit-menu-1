c204644a9109b73fa36474166d18a4344aba8f92
unknown <sachinreddu55@gmail.com>
2025-05-16 20:50:10 +0530

    second commit

--- src/Components/Home/MainCourse.jsx
+++ src/Components/Home/MainCourse.jsx
@@ -11,9 +11,40 @@ console.log(mainCourse)
   
   return (
     <>
-      <div>
+      <div className="w-full px-4 sm:px-6 md:px-8 py-8">
+        <div className="w-full text-center mb-8">
+          <h2 className="text-3xl sm:text-3xl md:text-4xl font-bold my-2">
+            {title}
+          </h2>
+          <h3 className="text-lg sm:text-sm md:text-base text-gray-600">
+            {served_time}
+          </h3>
+        </div>
+
+        {categories.map((food, index) => (
+          <div
+            key={index}
+            className="w-full flex flex-col items-center justify-center mb-12"
+          >
+            <h1 className="text-2xl sm:text-2xl md:text-3xl font-[oswald] font-semibold text-center my-6 uppercase">
+              {food.name}
+            </h1>
+
+            <div className="w-full flex flex-wrap justify-center gap-4 px-2 sm:px-0">
+              {food.items.map((item, idx) => (
+                <div
+                  key={idx}
+                >
+                  <Card item={item} />
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+      {/* <div>
         <div className="w-full text-center mb-6">
-            <h2 className="text-2xl sm:text-3xl md:text-4xl font-bold">{title}</h2>
+            <h2 className="text-3xl my-2 sm:text-3xl md:text-4xl font-bold">{title}</h2>
             <h3 className="text-sm sm:text-base md:text-lg">{served_time}</h3>
         </div>
         
@@ -28,7 +59,7 @@ console.log(mainCourse)
         </div>
        ))}
         
-      </div>
+      </div> */}
     </>
   )
 }
